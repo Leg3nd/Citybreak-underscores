0ea9e9b9ce56cd2a852687bd3001bf74dcfeb75d
Andrew Smallwood <Leg3nd@users.noreply.github.com>
2023-03-03 22:02:08 +0000

    Header update

--- functions.php
+++ functions.php
@@ -53,6 +53,24 @@ function citybreak_underscoresme_setup() {
 		)
 	);
 
+	// Function to update the li class in the primary menu. link_class is called in the header
+	function add_additional_class_on_li($classes, $item, $args) {
+		if(isset($args->add_li_class)) {
+			$classes[] = $args->add_li_class;
+		}
+		return $classes;
+	}
+	add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
+
+	// Function to update the anchor class in the primary menu. link_class is called in the header
+	function add_menu_link_class( $atts, $item, $args ) {
+		if (property_exists($args, 'link_class')) {
+		  $atts['class'] = $args->link_class;
+		}
+		return $atts;
+	  }
+	  add_filter( 'nav_menu_link_attributes', 'add_menu_link_class', 1, 3 );
+	
 	/*
 		* Switch default core markup for search form, comment form, and comments
 		* to output valid HTML5.
--- header.php
+++ header.php
@@ -19,7 +19,6 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
   	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   	<link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@1,600&family=Source+Sans+Pro&display=swap" rel="stylesheet">
-
 	<?php wp_head(); ?>
 </head>
 
@@ -30,38 +29,7 @@
 
 	<header id="masthead" class="site-header">
 
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$citybreak_underscoresme_description = get_bloginfo( 'description', 'display' );
-			if ( $citybreak_underscoresme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $citybreak_underscoresme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'citybreak-underscoresme' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-
-		<!-- Start of my header -->
+		<!-- Start of navigation -->
 		<div class="w-full p-4 bg-citybreakteal">
 			<nav class="container">
 				<!-- Flex Container -->
@@ -74,22 +42,41 @@
 					</div>
 					<!-- Menu items -->
 					<div class="hidden md:flex space-x-6">
-						<a href="#" class="font-inter text-lg uppercase text-white no-underline hover:underline">Destinations</a>
-						<a href="#" class="font-inter text-lg uppercase text-white no-underline hover:underline">Articles</a>
+					<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'menu-1',
+								'menu_id'        => 'primary-menu',
+								'menu_class'     => 'flex',
+								'add_li_class'  => 'mr-4 text-white',
+								'link_class'     => 'font-inter text-lg uppercase no-underline hover:underline',
+							)
+						);
+						?>
+						<p class="text-white">test</p>
 					</div>
 					
-				<!-- Hamburger menu -->
-				<button id="menu-btn" class="block hamburger md:hidden focus:outline-none">
-					<span class="hamburger-top"></span>
-					<span class="hamburger-middle"></span>
-					<span class="hamburger-bottom"></span>
-				</button>
+					<!-- Hamburger menu -->
+					<button id="menu-btn" class="block hamburger md:hidden focus:outline-none">
+						<span class="hamburger-top"></span>
+						<span class="hamburger-middle"></span>
+						<span class="hamburger-bottom"></span>
+					</button>
 				</div>
 				<!-- Mobile menu -->
 				<div class="md:hidden">
 					<div id="menu" class="absolute flex-col items-center self-end hidden py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md">
-						<a href="#" class="font-inter text-white hover:text-citybreakTeal600">Destinations</a>
-						<a href="#" class="font-inter text-white hover:text-citybreakTeal">Articles</a>
+					<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'menu-1',
+								'menu_id'        => 'primary-menu',
+								'menu_class'     => 'flex',
+								'add_li_class'  => 'mr-4',
+								'link_class'     => 'font-inter text-white hover:text-citybreakteal',
+							)
+						);
+						?>
 					</div>
 				</div>
 			</nav>
